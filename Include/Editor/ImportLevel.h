04aba824ae0dbcbc427cde6ea1855f7d596ab0db
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2024-10-07 17:06:49 +0900

    仮置きでjsonに入ってるデータを読み込み配列で返す関数を作成

--- Include/Editor/ImportLevel.h
+++ Include/Editor/ImportLevel.h
@@ -1,1 +1,44 @@
 #pragma once
+//レベルデータを読み込んで返すクラス
+
+#include "DxLib.h"
+#include "Vector2.h"
+#include "vector"
+#include <memory>
+
+//jsonの読み込みとかに使う
+#pragma warning( push )
+#pragma warning( disable : 26819 )
+#include "json.hpp"
+#pragma warning( pop )
+
+struct LevelData
+{
+	//基本情報
+	Vector2 pos;
+	Vector2 scale;
+
+	//中身(後でenumに変えるかも)
+	std::string tag = "";
+};
+
+class ImportLevel
+{
+public:
+	ImportLevel();
+	~ImportLevel();
+
+	//取ってきたデータを既定の配列に入れる
+	//データをvector配列に入れる
+	static std::vector<LevelData> ImportLevelVectorData(const std::string& fileName);
+	//データをlist配列に入れる
+	static std::list<LevelData> ImportLevelListData(const std::string& fileName);
+
+	//レベルの中身読み込むよう
+	static LevelData LevelScanning(nlohmann::json& Level);
+
+
+private:
+
+};
+
---
+++ Source/Editor/ImportLevel.cpp
@@ -0,0 +1,124 @@
+#include "ImportLevel.h"
+#include <sstream>
+#include <fstream>
+#include <cstdint>
+#include<string>
+
+ImportLevel::ImportLevel()
+{
+}
+
+ImportLevel::~ImportLevel()
+{
+}
+
+std::vector<LevelData> ImportLevel::ImportLevelVectorData(const std::string& fileName)
+{
+
+	std::vector<LevelData> levelData;
+	//ファイルストリーム
+	std::ifstream file(fileName);
+
+	if (!file)
+	{
+		assert(0);
+	}
+
+	//JSON文字列から解凍したデータ
+	nlohmann::json deserialized;
+
+	//解凍
+	file >> deserialized;
+
+	//正しいイベントファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	//"name"を文字列として取得
+	std::string name = deserialized["name"].get<std::string>();
+
+	//中身が正しいかどうかチェック
+	assert(name.compare("Level") == 0);
+
+	//"events"の全オブジェクトを走査
+	for (nlohmann::json& objects : deserialized["objects"])
+	{
+
+		levelData.push_back(LevelScanning(objects));
+	}
+	
+	if (levelData.empty())
+	{
+		levelData.push_back(LevelData());
+	}
+
+	return levelData;
+}
+
+std::list<LevelData> ImportLevel::ImportLevelListData(const std::string& fileName)
+{
+	std::list<LevelData> levelData;
+	//ファイルストリーム
+	std::ifstream file(fileName);
+
+	if (!file)
+	{
+		assert(0);
+	}
+
+	//JSON文字列から解凍したデータ
+	nlohmann::json deserialized;
+
+	//解凍
+	file >> deserialized;
+
+	//正しいイベントファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	//"name"を文字列として取得
+	std::string name = deserialized["name"].get<std::string>();
+
+	//中身が正しいかどうかチェック
+	assert(name.compare("Level") == 0);
+
+	//"events"の全オブジェクトを走査
+	for (nlohmann::json& objects : deserialized["objects"])
+	{
+
+		levelData.push_back(LevelScanning(objects));
+	}
+
+	if (levelData.empty())
+	{
+		levelData.push_back(LevelData());
+	}
+
+	return levelData;
+}
+
+LevelData ImportLevel::LevelScanning(nlohmann::json& Level)
+{
+
+
+	LevelData levelData;
+
+	//設定のパラメータ読み込み
+	nlohmann::json& seting = Level["object"];
+
+	//移動する場所読み込み
+	levelData.pos.x = (float)seting["pos"][0];
+	levelData.pos.y = (float)seting["pos"][1];
+
+	//移動開始位置取得
+	levelData.scale.x = (float)seting["scale"][0];
+	levelData.scale.y = (float)seting["scale"][1];
+
+	//スピードのセット
+	levelData.tag = seting["tag"].get<std::string>();
+
+	return levelData;
+	
+}
