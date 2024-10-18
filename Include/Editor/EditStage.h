1d6171b432586748753ef84afbe8532a68d9ad2b
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2024-10-18 14:04:53 +0900

    大体準備は終わったけど追加方法がガバガバなのでちょっと考えます

--- Include/Editor/EditStage.h
+++ Include/Editor/EditStage.h
@@ -1,5 +1,34 @@
 #pragma once
 #include "StageManager.h"
+#include <imgui.h>
+
+namespace ImGui
+{
+#include "vector"
+	bool DragFloat2(const char* label, Vector2 v, float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f", ImGuiSliderFlags flags = 0);
+	bool Combo(const std::string& comboName, int32_t& number, std::vector<std::string> items);
+}
+
+struct ObjectName
+{
+	static ObjectType ObjectString(int32_t objectNum)
+	{
+		if (objectNum == static_cast<int32_t>(ObjectType::PLAYER))
+		{
+			return ObjectType::PLAYER;
+		}
+		else if (objectNum == static_cast<int32_t>(ObjectType::FLOAT_BLOCK))
+		{
+			return ObjectType::FLOAT_BLOCK;
+		}
+		else if (objectNum == static_cast<int32_t>(ObjectType::NOT_FLOAT_BLOCK))
+		{
+			return ObjectType::NOT_FLOAT_BLOCK;
+		}
+
+		return ObjectType::NONE;
+	}
+};
 
 class EditStage
 {
@@ -24,16 +53,27 @@ private:
 	//エディタ用の更新
 	void EditorUpdate();
 
+	void ImguiMenu();
+
 	void addObject();
 
 	void SaveAndLoadLevelObject();
 
+	std::string ObjectTypeToString(ObjectType objectType);
+
 
 private:
 
-	ObjectType serectAddObjectType_;
+	ObjectType serectAddObjectType_= ObjectType::NONE;
 
+	Vector2 AddObjectPos_ = { 1,1 };
 	Vector2 AddObjectSize_ = { 1,1 };
+
+	InputLevelData loadData;
+
+	bool imguiSaveWindow_ = false;
+	bool imguiLoadWindow_ = false;
+	bool imguiAddObjectWindow_ = false;
 
 };
 
--- Source/Editor/EditStage.cpp
+++ Source/Editor/EditStage.cpp
@@ -1,7 +1,28 @@
 #include "EditStage.h"
 #include "DxLib.h"
 #include <fstream>
-#include <imgui.h>
+
+#include "Input.h"
+
+bool ImGui::DragFloat2(const char* label, Vector2 v, float v_speed, float v_min, float v_max, const char* format, ImGuiSliderFlags flags)
+{
+	float vf[2] = { v.x,v.y };
+	bool result = false;
+	result= ImGui::DragFloat2(label, vf, v_speed, v_min, v_max, format, flags);
+	return result;
+}
+
+bool ImGui::Combo(const std::string& comboName, int32_t& number, std::vector<std::string> items)
+{
+	std::vector<const char*> tmp;
+
+	for (const auto& item : items)
+	{
+		tmp.push_back(item.c_str());
+	}
+
+	return ImGui::Combo(comboName.c_str(), &number, tmp.data(), int32_t(items.size()));
+}
 
 EditStage::EditStage()
 {
@@ -29,13 +50,90 @@ void EditStage::Draw()
 void EditStage::EditorUpdate()
 {
 	addObject();
+
+	ImguiMenu();
+}
+
+void EditStage::ImguiMenu()
+{
+	//imguiの機能を追加するためのフラグ
+	ImGuiWindowFlags window_flags = 0;
+	//メニューバーを使います
+	window_flags |= ImGuiWindowFlags_MenuBar;
+	//beginで渡すことでフラグをこのウインドウで有効にする
+	ImGui::Begin("Editor", NULL, window_flags);
+
+
+	//エディタの機能をまとめたメニュー
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File\n"))
+		{
+			ImGui::MenuItem("EventSave", NULL, &imguiSaveWindow_);
+			ImGui::MenuItem("EventLoad", NULL, &imguiLoadWindow_);
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("addEvent\n"))
+		{
+			imguiAddObjectWindow_ = true;
+			ImGui::EndMenu();
+		}
+
+		ImGui::Text("%0.0fFPS", ImGui::GetIO().Framerate);
+		ImGui::EndMenuBar();
+	}
+
+	SaveAndLoadLevelObject();
+
+	ImGui::End();
 }
 
 void EditStage::addObject()
 {
-	if (!ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow))
+
+	if (ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow))
 	{
-		StageManager::GetInstance()->AddObject({ 0,0 }, AddObjectSize_, serectAddObjectType_);
+		AddObjectPos_ = Input::GetMousePos();
+	}
+
+	if (!imguiAddObjectWindow_)return;
+
+	if (!ImGui::Begin("addObject", &imguiAddObjectWindow_))
+	{
+		ImGui::End();
+		return;
+	}
+
+	std::vector<std::string>items;
+	items.resize(static_cast<int32_t>(ObjectType::NONE));
+
+	int32_t oldObjectType = static_cast<int32_t>(serectAddObjectType_);
+
+	for (size_t i = 0; i < items.size(); i++)
+	{
+		items[i] = ObjectTypeToString(ObjectType(i));
+	}
+	int32_t objectType = static_cast<int32_t>(serectAddObjectType_);
+
+	//設定したいイベントの番号にする
+	ImGui::Combo("object Type", objectType, items);
+
+	if (oldObjectType != objectType)
+	{
+		serectAddObjectType_ = ObjectName::ObjectString(objectType);
+	}
+
+	ImGui::DragFloat2("Pos", AddObjectPos_, 1.0f, -1000.0f, 1000.0f);
+	ImGui::DragFloat2("Size", AddObjectSize_, 1.0f, 1.0f, 1000.0f);
+
+	if (ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow) && ImGui::Button("addObject"))
+	{
+		StageManager::GetInstance()->AddObject(AddObjectPos_, AddObjectSize_, serectAddObjectType_);
+	}
+
+	if (!ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow) && Input::GetMouseKeyTrigger(Input::MouseKey::LEFT))
+	{
+		StageManager::GetInstance()->AddObject(Input::GetMousePos(), AddObjectSize_, serectAddObjectType_);
 	}
 
 }
@@ -171,21 +269,20 @@ void EditStage::SaveLevelFullPathData(const std::string& fileName, const std::li
 
 void EditStage::SaveAndLoadLevelObject()
 {
-	ImGui::Begin("saveAndLoad LevelData");
 
-
-	if (ImGui::Button("saveFile"))
+	if (imguiSaveWindow_)
 	{
-
 		WindowsSaveFile(StageManager::GetInstance()->GetObjectList());
-
+		imguiSaveWindow_ = false;
 	}
-	InputLevelData loadData;
-	if (ImGui::Button("openFile"))
+	if (imguiLoadWindow_)
 	{
-		
 		loadData = ImportLevel::GetInstance()->WindowsOpenLevelFileList();
-
+		if (loadData.isLoad)
+		{
+			StageManager::GetInstance()->LoadListStageData(loadData.levelData);
+		}
+		imguiLoadWindow_ = false;
 	}
 
 	if (!loadData.isLoad)
@@ -193,8 +290,16 @@ void EditStage::SaveAndLoadLevelObject()
 		ImGui::Text("Loading failed");
 		ImGui::Text(std::string("reason " + ImportLevel::GetInstance()->GetLoadErrorText()).c_str());
 	}
+}
 
-	StageManager::GetInstance()->LoadListStageData(loadData.levelData);
 
-	ImGui::End();
+
+std::string EditStage::ObjectTypeToString(ObjectType objectType)
+{
+	switch (objectType) {
+	case ObjectType::PLAYER:   return "player";
+	case ObjectType::FLOAT_BLOCK: return "floatBlock";
+	case ObjectType::NOT_FLOAT_BLOCK: return "notFloatBlock";
+	default:    return "UNKNOWN";
+	}
 }
