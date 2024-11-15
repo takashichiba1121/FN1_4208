928c0b3195dc6196bd2e7fd2669e127cb728e07a
takashithiba <k021g1120@g.neec.ac.jp>
2024-11-15 11:53:29 +0900

    当たり判定のバグ修正

--- Source/Collision/CollisionManager.cpp
+++ Source/Collision/CollisionManager.cpp
@@ -8,7 +8,7 @@ void CollisionManager::AddObject(Object* object)
 
 void CollisionManager::RemoveObject(Object* object)
 {
-  	objects_.remove(object);
+	objects_.remove(object);
 }
 
 void CollisionManager::Update()
@@ -39,9 +39,9 @@ void CollisionManager::Update()
 
 			if (Collision::AABB(objectA, objectB))
 			{
- 				if (objectA->IsExclude()&&objectB->IsExclude())
+				if (objectA->IsExclude() && objectB->IsExclude())
 				{
- 					Vector2 posB = objectB->GetPos();
+					Vector2 posB = objectB->GetPos();
 					Vector2 sizeB = objectB->GetSize();
 					Vector2 oldPosB = objectB->GetOldPos();
 
@@ -49,58 +49,65 @@ void CollisionManager::Update()
 					Vector2 sizeA = objectA->GetSize();
 					Vector2 oldPosA = objectA->GetOldPos();
 
+					Vector2 moveA = { 0,0 };
+
+					Vector2 moveB = { 0,0 };
+
+
+
+					if (posA.y < posB.y)
+					{
+						moveA.y = (posA.y + sizeA.y / 2)- (posB.y - sizeB.y / 2);
+
+						moveB.y = (oldPosA.y + sizeA.y / 2)- (oldPosB.y - sizeB.y / 2);
+					}
+					else
+					{
+						moveA.y = (posA.y - sizeA.y / 2) - (posB.y + sizeB.y / 2);
+
+						moveB.y = (oldPosA.y - sizeA.y / 2) - (oldPosB.y + sizeB.y / 2);
+					}
+
+					if (posA.x < posB.x)
+					{
+						moveA.x = (posA.x + sizeA.x / 2)- (posB.x - sizeB.x / 2);
+
+						moveB.x = (oldPosA.x + sizeA.x / 2)- (oldPosB.x - sizeB.x / 2);
+					}
+					else
+					{
+						moveA.x = (posA.x - sizeA.x / 2) - (posB.x + sizeB.x / 2);
+
+						moveB.x = (oldPosA.x - sizeA.x / 2) - (oldPosB.x + sizeB.x / 2);
+					}
+
+					if (moveA.x > 0 && moveB.x > 0 || moveA.x < 0 && moveB.x < 0)
+					{
+						moveA.x = 0;
+
+						moveB.x = 0;
+					}
+					if (moveA.y > 0 && moveB.y > 0 || moveA.y < 0 && moveB.y < 0)
+					{
+						moveA.y = 0;
+
+						moveB.y = 0;
+					}
+
 					if (objectA->GetObjectType() < objectB->GetObjectType())
 					{
-						if (posB.y - sizeB.y/2 >= oldPosA.y + sizeA.y/2)
-						{
-							posA.y = posB.y - (sizeB.y/2 + sizeA.y/2);
-						}
-						else if (posB.y + sizeB.y/2 <= oldPosA.y - sizeA.y/2)
-						{
-							posA.y = posB.y + sizeB.y/2 + sizeA.y/2;
-						}
-						else
-						{
 
-							if (posB.x + sizeB.x/2 <= oldPosA.x - sizeA.x/2)
-							{
-								posA.x = posB.x + sizeB.x/2 + sizeA.x/2 + 0.1f;
-							}
-							else if (posB.x - sizeB.x/2 >= oldPosA.x + sizeA.x/2)
-							{
-								posA.x = posB.x - (sizeB.x/2 + sizeA.x/2) - 0.1f;
+						posA += moveA;
 
-							}
-						}
 						objectA->SetPos(posA);
 					}
 					else
 					{
 
-						if (posA.y - sizeA.y/2 >= oldPosB.y + sizeB.y/2)
-						{
-							posB.y = posA.y - (sizeA.y/2 + sizeB.y/2);
-						}
-						else if (posA.y + sizeA.y/2 <= oldPosB.y - sizeB.y/2)
-						{
-							posB.y = posA.y + sizeA.y/2 + sizeB.y/2;
-						}
-						else
-						{
-
-							if (posA.x + sizeA.x/2 <= oldPosB.x - sizeB.x/2)
-							{
-								posB.x = posA.x + sizeA.x/2 + sizeB.x/2;
-							}
-							else if (posA.x - sizeA.x/2 >= oldPosB.x + sizeB.x/2)
-							{
-								posB.x = posA.x - (sizeA.x/2 + sizeB.x/2);
-
-							}
-						}
-					}
+						posB += moveA;
 
 						objectB->SetPos(posB);
+					}
 				}
 
 				objectA->OnCollision(objectB);
--- Source/Stage/Item/LevitationBlock.cpp
+++ Source/Stage/Item/LevitationBlock.cpp
@@ -8,7 +8,7 @@
 void LevitationBlock::Initialize()
 {
 	// èâä˙íl
-	pos_ = { 640.0f,100.0f };
+	pos_ = { 100.0f,640.0f };
 	size_ = { 64.0f,64.0f };
 	gravity_ = 10.0f;
 
