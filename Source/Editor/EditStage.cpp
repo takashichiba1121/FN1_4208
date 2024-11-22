9294102e57a2517d70aa95c0534c9ab91ff75356
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2024-11-22 16:42:27 +0900

    横幅調整

--- Source/Editor/EditStage.cpp
+++ Source/Editor/EditStage.cpp
@@ -184,6 +184,11 @@ void EditStage::addObject()
 		{
 			serectAddObjectType_ = ObjectName::ObjectString<ObjectType>(i);
 		}
+		if (i != 0 && i % 4 == 0)
+		{
+			continue;
+		}
+
 		if (i != items.size()-1)
 		{
 			ImGui::SameLine();
