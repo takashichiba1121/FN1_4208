3397420f13e023cc92156f4e6945f2a374999867
takashithiba <k021g1120@g.neec.ac.jp>
2024-11-22 16:41:56 +0900

    関数名変更

--- Include/Collision/Object.h
+++ Include/Collision/Object.h
@@ -59,7 +59,7 @@ public:
 		tentSize_ = size_.y;
 	}
 
-	void SetSize(Vector2 size) {
+	 void SetSize(Vector2 size) {
 		size_ = size;
 	}
 
@@ -91,7 +91,7 @@ public:
 
 	virtual void GetJson(nlohmann::json& Level) {};
 
-	virtual void DragFloat2() {};
+	virtual void ImGuiEdit() {};
 
 protected:
 	Vector2 pos_ = { 0,0 };
