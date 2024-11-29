bba8c1957cd3a1b14bb0d54bb6da4fa66238f638
TakatoOogami <k021g1156@g.neec.ac.jp>
2024-11-29 12:31:40 +0900

    タイトル画面の演出追加

--- Source/Scene/TitleScene.cpp
+++ Source/Scene/TitleScene.cpp
@@ -1,15 +1,42 @@
 #include "TitleScene.h"
+#include "SceneManager.h"
+#include "TextureManager.h"
+#include "Input.h"
+#include <DxLib.h>
 
 void TitleScene::Initialize()
 {
+	textruehandle_ = TextureManager::Instance()->LoadTexture("Resources\\Texture\\PressSpace.png");
+
+	for (int i = 0; i < 3; i++) {
+
+		titleLogo[i] = std::make_unique<TitleLogo>();
+		titleLogo[i]->Initialize(i);
+	}
 }
 
 void TitleScene::Update()
 {
+	if (Input::GetKeyTrigger(Input::Key::Space)) {
+		SceneManager::GetInstance()->ChangeScene("STAGESELECT");
+	}
+
+	for (int i = 0; i < 3; i++) {
+		titleLogo[i]->SetPos({WIN_WIDTH / 2 + 300 * (float)(i - 1) ,Water::GetInstance()->GetHorizontal()});
+		titleLogo[i]->Update();
+	}
+	Water::GetInstance()->Update();
 }
 
 void TitleScene::Draw()
 {
+	for (std::unique_ptr<TitleLogo>& logo : titleLogo) {
+		logo->Draw();
+	}
+	
+	Water::GetInstance()->Draw();
+
+	DrawGraph(WIN_WIDTH / 2 - 256, 550, textruehandle_,true);
 }
 
 void TitleScene::Finalize()
